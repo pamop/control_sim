5f41503bece0e053a37426646a7c1040c17c9e3c
Pam Osborn Popp <pamop@nyu.edu>
2017-04-17 10:33:49 -0400

    Developing sim2 averaging over simulations and plotting

--- sim2.py
+++ sim2.py
@@ -7,19 +7,21 @@ Created on Thu Apr 13 11:55:48 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
+import time
 
 # Task parameters
 nactions = 5
 noutcomes = nactions
 L = nactions # variable name in paper
 M = np.zeros([L,L])
-numM = 1 # |M|
+numM = 5 # |M|
 M[:numM,:numM] = np.identity(numM)
 R = [0.3,0,0,0,0.7] #[0,0.2,0.23,0.27,0.3] # Sum to 1
 c = 0.8
 
 ntrials = 200
-nsubj = 50
+nsubj = 300
 
 cbar = (1-c)/(L-1)
 C = cbar * np.ones([L,L])
@@ -58,19 +60,19 @@ class simsubj(object):
         
 # Run simulated experiment
 nLRcases = 3
-avgrwd = np.zeros(nLRcases,ntrials) 
+avgrwd = np.zeros([nLRcases,ntrials]) 
  
 for LRcase in range(0,nLRcases):  
     
     
-    actions = np.zeros(nsubj,ntrials)
-    outcomes = np.zeros(nsubj,ntrials)
-    trial_rwds = np.zeros(nsubj,ntrials)
+    actions = np.zeros([nsubj,ntrials])
+    outcomes = np.zeros([nsubj,ntrials])
+    trial_rwds = np.zeros([nsubj,ntrials])
     
     for subject in range(0,nsubj):
         # Simulated subject parameters
         alpha = 0.01 * 10**LRcase
-        beta = 0.01 * 10**LRcase
+        beta = 0.01
         subj = simsubj(L,alpha,beta)
     
     
@@ -89,7 +91,43 @@ for LRcase in range(0,nLRcases):
             outcomes[subject,trial] = outcome
             trial_rwds[subject,trial] = trial_rwd
             
-        avgrwd[LRcase,:] = np.mean(trial_rwds,axis=0)
-        
+    avgrwd[LRcase,:] = np.mean(trial_rwds,axis=0)
+    
 # Visualize
-plt.plot(avgrwd[])
+plt.plot(avgrwd[0,:],'ro',avgrwd[1,:],'bo',avgrwd[2,:],'go')
+plt.xlabel('trial')
+plt.ylabel('average reward')
+plt.legend(('LR=0.01','LR=0.1','LR=1.0'),loc='lower right')
+
+#plt.plot(actions[1,:],'o')
+plt.plot(np.mean(actions,axis=0),'o')
+
+## hist
+#observations = [range(10),range(20),range(50),range(200)]
+#filenames = []
+#for ii in range(0,4):
+##    subjhist, edges = np.histogram(actions[:,ii*50:ii*50+49],bins=[0, 1, 2, 3, 4])
+#    plt.hist(actions[1,observations[ii]],bins=5)
+#    fname = 'hist' + str(ii)
+#    filenames.append(fname)
+#    plt.title('|M|=4,LR=1')
+#    plt.savefig(fname)
+#    plt.cla()
+##    time.sleep(1)
+
+
+#filenames = []
+#for ii in range(0,4):
+#    plt.hist(np.mean(actions[:,ii*50:ii*50 + 49],axis=0),bins=5)
+#    fname = 'hist' + str(ii)
+#    filenames.append(fname)
+#    plt.savefig(fname)
+#    plt.cla()
+#
+##stats.binned_statistic(np.ones(ntrials), np.mean(actions,axis=0), 'mean', bins=20)
+#
+#import imageio
+#images = []
+#for filename in filenames:
+#    images.append(imageio.imread(filename))
+#imageio.mimsave('/Users/pamop/Documents/gureckis lab/control_sim/test.gif', images)
